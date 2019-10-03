cdf15be12257e61908121faa00f431f686ac603d
Ethan <egruenemeier984@west-mec.org>
2019-10-03 15:26:01 -0700

    created Fetch file
    created problems with the express server.

--- Projects/express.js
+++ Projects/express.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', function(req, res) {
+app.get('/home', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.get('/', function(req, res) {
+    res.redirect('/index.html')
+})
+
+
 
 app.listen(5000);
--- Projects/fetchData.js
+++ Projects/fetchData.js
@@ -1,6 +1,14 @@
-const starWars = require('swapi-node');
-
-starWars.get('https://swapi.co/api/people/').then((result) => {
-    const people = result.name;
-    console.log(people); 
-});
+fetch('https://pokeapi.co/api/v2/pokemon/5')
+.then( response => response.json() )
+.then( 
+    data => {
+        const {...pokemon} = data.sprites;
+        const {...type} = data.species;
+        console.log( pokemon );
+        console.log( type.name );
+        selector = document.querySelector('.pokemon');
+        selectorName = document.querySelector('.name');
+        selector.innerHTML = `<img height="500px" width="500px" src="${pokemon.front_shiny}" />`
+        selectorName.innerHTML = `<h3>Name: ${type.name} </h3> `
+    }
+);
