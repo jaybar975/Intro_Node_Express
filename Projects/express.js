2145c7e117d241330cf229cc9b5158ac62876400
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2019-10-04 12:19:12 -0700

    loads js with express
    now the fetch js works with the express server.

--- Projects/express.js
+++ Projects/express.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/home', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/', function(req, res) {
-    res.redirect('/index.html')
-})
+app.get('/fetchData.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/fetchData.js'));
+});
 
+app.get('/', function(req, res) {
+    res.redirect('/home')
+})
 
 
 app.listen(5000);
